4793cb67836aa705f78886f6f14326a5bec222ed
Retro Alister <retroali@ga-q4.msk.21-school.ru>
2024-01-27 17:48:02 +0300

    merge3

--- P01D06/src/main.c
+++ P01D06/src/main.c
@@ -2,6 +2,8 @@
 
 void logic();
 void update();
+void horizonDraw();
+void vierticalDraw();
 
 int main() {
     int flag = 1;
@@ -20,3 +22,26 @@ int main() {
         update();
     }
 }
+
+void update() {
+    horizonDraw();
+    vierticalDraw();
+    horizonDraw();
+}
+
+void horizonDraw() {
+    char viewGor = '-';
+    for (int i = 0; i <= 80; ++i) {
+        printf("%c", viewGor);
+    }
+    printf("\n");
+}
+
+void vierticalDraw() {
+    char viewVer = '|';
+    for (int i = 0; i < 25; ++i) {
+        printf("%c", viewVer);
+        for (int j = 0; j < 80 - 1; ++j) printf(" ");
+        printf("%c", viewVer);
+        printf("\n");
+    }
