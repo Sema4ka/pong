a2bf3d39f7db13b69ccbe367a2e4803ce31ea1a0
Lacie Phaedra <laciepha@ga-q3.msk.21-school.ru>
2024-01-27 19:48:40 +0300

    unukh Отражение мяча и ракеток

--- P01D06/src/main.c
+++ P01D06/src/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-void logic();
+//void logic();
 void update();
 void horizonDraw();
 void vierticalDraw();
+int racketDraw(int, int);
+int ballDraw(int, int);
 
 int main() {
     int flag = 1;
@@ -17,10 +19,10 @@ int main() {
     int rocket2_y = 10;
     int rocket1_x = 20;
     int rocket2_x = 60;
-    while (flag) {
-        logic();
+    //while (flag) {
+        //logic();
         update();
-    }
+    //}
 }
 
 void update() {
@@ -39,9 +41,64 @@ void horizonDraw() {
 
 void vierticalDraw() {
     char viewVer = '|';
+    int ballDrawFlag = 0;
+    
     for (int i = 0; i < 25; ++i) {
         printf("%c", viewVer);
-        for (int j = 0; j < 80 - 1; ++j) printf(" ");
+        for (int j = 0; j < 80 - 1; ++j)
+        {
+            if( ! racketDraw(i, j))
+                printf(".");
+            
+            if( ! ballDraw(i, j))
+                printf("#");
+          
+        }
         printf("%c", viewVer);
         printf("\n");
     }
+
+}
+
+int racketDraw(int row, int column){
+    int flag = 0;
+    int firstCoordinate = 11;
+    int leftOrRight = 0;
+    
+    if(column > 40)
+        leftOrRight = 77;
+    else
+        leftOrRight = 1;
+        
+    
+    if((row == firstCoordinate && column == leftOrRight))
+    {
+        printf("|");
+        flag = 1;
+    }
+    if((row == firstCoordinate + 1 && column == leftOrRight))
+    {
+        printf("|");
+        flag = 1;
+    }
+    if((row == firstCoordinate + 2 && column == leftOrRight))
+    {
+        printf("|");
+        flag = 1;
+    }
+    
+    return flag;
+}
+
+int ballDraw(int row, int column)
+{
+    int flag = 0;
+    if((row == 10) && (column == 10))
+    {
+        printf("*");
+        flag = 1;
+    }
+    
+    return flag;
+    
+}
