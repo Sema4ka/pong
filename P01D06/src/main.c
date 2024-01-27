b4275184ec0f75f479c8c3f3b13a1572ebbf002c
Retro Alister <retroali@ga-q4.msk.21-school.ru>
2024-01-27 18:53:52 +0300

    collision v1

--- P01D06/src/main.c
+++ P01D06/src/main.c
@@ -4,6 +4,9 @@ void gameLogic();
 void gameUpdate();
 void horizonDraw();
 void vierticalDraw();
+int ballMoveX(int, int);
+int ballMoveY(int, int);
+int checkBallRocket(int, int, int, int, int, int);
 
 int main() {
     int flag = 1;
@@ -17,13 +20,37 @@ int main() {
     int rocket2_y = 10;
     int rocket1_x = 20;
     int rocket2_x = 60;
+
+    int tmp;
     while (flag) {
-        gameLogic();
+        ball_x = ballMoveX(ball_velGor, ball_x);
+        ball_y = ballMoveY(ball_velVer, ball_y);
+        tmp = checkBallRocket(ball_x, ball_y, rocket1_x, rocket1_y, rocket2_x, rocket2_y);
+        ball_velVer *= tmp;
+        ball_velGor *= tmp;
         gameUpdate();
     }
 }
 
-void gameLogic(){}
+int ballMoveX(int velG, int x){
+   return (velG + x);
+};
+
+
+int ballMoveY(int velV, int y){
+   return (velV + y);
+};
+
+int checkBallRocket(int x, int y, int r1x, int r1y, int r2x, int r2y){
+    int checkR1 = (x == r1x && (y == r1y || y ==r1y-1 || y == r1y-2));
+    int checkR2 = (x == r2x && (y == r2y || y ==r2y-1 || y == r2y-2));
+
+    if(checkR1==0 || checkR2==0)
+        return -1;
+
+    else 
+        return 1;
+}
 
 void gameUpdate() {
     horizonDraw();
