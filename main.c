9188221ba0928152cfb925e5df9dbcb3d60bc137
Lacie Phaedra <laciepha@ga-q3.msk.21-school.ru>
2024-01-27 17:38:32 +0300

    playGround update

--- main.c
+++ main.c
@@ -1,35 +1,34 @@
-# include <stdio.h>
+#include <stdio.h>
 
-void logic();
 void update();
+void horizonDraw();
+void vierticalDraw();
 
-struct Ball
-{
-    int transform;
-    int velUp;
-    int velForward;
-    char view ='*';
-} ball;
-
-struct Rocket{
-    int transform;
-    char view = "|";
-}
-
-struct PlayGround
-{
-    char coord[80][25];
-    struct Ball ball;
-    struct Rocket rocket1;
-    struct Rocket rocket2;
-    char viewVer ='|';
-    char viewGor ='-';
-}
-
-int main(){
+int main() {
     int flag = 1;
-    while(flag){
-        logic();
-        update();
+    while (flag) update();
+}
+
+void update() {
+    horizonDraw();
+    vierticalDraw();
+    horizonDraw();
+}
+
+void horizonDraw() {
+    char viewGor = '-';
+    for (int i = 0; i <= 80; ++i) {
+        printf("%c", viewGor);
+    }
+    printf("\n");
+}
+
+void vierticalDraw() {
+    char viewVer = '|';
+    for (int i = 0; i < 25; ++i) {
+        printf("%c", viewVer);
+        for (int j = 0; j < 80 - 1; ++j) printf(" ");
+        printf("%c", viewVer);
+        printf("\n");
     }
 }
